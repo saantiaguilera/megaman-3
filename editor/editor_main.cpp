4fb5b7ba2eef70871bd1f292ea4d73f61da2b891
santilazzari <santilazzari1994@gmail.com>
2016-05-14 18:03:11 -0300

    GTKMM Working for editor and creation of main editor

---
+++ editor/editor_main.cpp
@@ -0,0 +1,17 @@
+/*
+ * editor_main.cpp
+ *
+ *  Created on: May 14, 2016
+ *      Author: santi
+ */
+
+#include <gtkmm.h>
+
+int main(int argc, char *argv[]) {
+  Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
+
+  Gtk::Window window;
+  window.set_default_size(1000, 1000);
+
+  return app->run(window);
+}
