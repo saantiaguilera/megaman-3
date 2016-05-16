31dfb2f52063208571d9266e5ff7ccd033fa8a95
Santi <aguilerasanti@hotmail.com>
2016-05-16 00:49:48 -0300

    A nicier UI

--- client/client_main.cpp
+++ client/client_main.cpp
@@ -11,8 +11,6 @@
 
 #define LAYOUT_PATH "res/layout/home_screen.glade"
 
-#include <gtk/gtk.h>
-
 int main(int argc, char **argv) {
 	GtkBuilder *builder;
 	GtkWidget *window;
@@ -24,16 +22,16 @@ int main(int argc, char **argv) {
 	/* Create new GtkBuilder object */
 	builder = gtk_builder_new();
 
-	/* Load UI from file. If error occurs, report it and quit application.
-	 * Replace "tut.glade" with your saved project. */
+	/* Load UI from file. If error occurs, report it and quit application.*/
 	if (!gtk_builder_add_from_file(builder, LAYOUT_PATH, &error)) {
 		g_warning("%s", error->message);
 		g_free(error);
-		return (1);
+
+		return 1;
 	}
 
 	/* Get main window pointer from UI */
-	window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "client_home_screen_root_view"));
 
 	/* Connect signals */
 	gtk_builder_connect_signals(builder, NULL);
@@ -49,3 +47,47 @@ int main(int argc, char **argv) {
 
 	return (0);
 }
+
+/*
+ Signal example:
+
+#include <gtk/gtk.h>
+
+static void print_hello(GtkWidget *widget, gpointer data) {
+	g_print("Hello World\n");
+}
+
+static void activate(GtkApplication *app, gpointer user_data) {
+	GtkWidget *window;
+	GtkWidget *button;
+	GtkWidget *button_box;
+
+	window = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window), "Window");
+	gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+
+	button_box = gtk_button_box_new(GTK_ORIENTATION_HORIZONTAL);
+	gtk_container_add(GTK_CONTAINER(window), button_box);
+
+	button = gtk_button_new_with_label("Hello World");
+	g_signal_connect(button, "clicked", G_CALLBACK (print_hello), NULL);
+	g_signal_connect_swapped(button, "clicked", G_CALLBACK (gtk_widget_destroy),
+			window);
+	gtk_container_add(GTK_CONTAINER(button_box), button);
+
+	gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+	GtkApplication *app;
+	int status;
+
+	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+	g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+	status = g_application_run(G_APPLICATION(app), argc, argv);
+	g_object_unref(app);
+
+	return status;
+}
+
+ */
