5b3d1b7e2334ed531a200aa87b9fd93d033eca86
Martin Stancanelli <mastanca14@gmail.com>
2016-05-20 23:53:03 -0300

    Add header with protocol

---
+++ common/common_MessageProtocol.h
@@ -0,0 +1,26 @@
+/*
+ * common_MessageProtocol.h
+ *
+ *  Created on: May 20, 2016
+ *      Author: mastanca
+ */
+
+#ifndef COMMON_COMMON_MESSAGEPROTOCOL_H_
+#define COMMON_COMMON_MESSAGEPROTOCOL_H_
+
+#define PLAYER_CONNECTED 01
+#define NEW_PLAYER 02
+#define START_GAME 03
+#define SEND_MAP 04
+#define KEY_PRESSED 05
+#define UPDATE_MOVEMENTS 06
+#define OBJECT_CREATED 07
+#define OBJECT_DESTROYED 08
+#define HP_CHANGE 09
+#define AMMO_CHANGE 10
+#define WEAPON_CHANGE 11
+#define END_GAME 12
+#define LIFE_CHANGE 13
+
+
+#endif /* COMMON_COMMON_MESSAGEPROTOCOL_H_ */
