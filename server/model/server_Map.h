bbc41002a7c330ae752c6d8532fe7b63e8d88dc4
Martin Stancanelli <mastanca14@gmail.com>
2016-05-21 21:22:49 -0300

    Add tile and map classes

---
+++ server/model/server_Map.h
@@ -0,0 +1,30 @@
+/*
+ * server_Map.h
+ *
+ *  Created on: May 21, 2016
+ *      Author: mastanca
+ */
+
+#ifndef SERVER_MODEL_SERVER_MAP_H_
+#define SERVER_MODEL_SERVER_MAP_H_
+
+#include <vector>
+
+#include "sever_Tile.h"
+
+class Map {
+private:
+	std::vector< std::vector< Tile* > > tilesMatrix;
+public:
+	// Constructor, creates a map of sizeX*sizeY tiles
+	Map(unsigned int sizeX, unsigned int sizeY);
+	// Destroyer
+	virtual ~Map();
+private:
+	// Copy constructor
+	Map(const Map&);
+	// Assignment operator
+	Map& operator=(const Map&);
+};
+
+#endif /* SERVER_MODEL_SERVER_MAP_H_ */
