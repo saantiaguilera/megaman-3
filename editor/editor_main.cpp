4d46b9dbfa608927d542a4dfd15bcb16e3ed9cd2
santilazzari <santilazzari1994@gmail.com>
2016-05-24 19:13:46 -0300

    Added a folder statement that have a copy of the instructions of the final work

--- editor/editor_main.cpp
+++ editor/editor_main.cpp
@@ -4,7 +4,7 @@
  *  Created on: May 14, 2016
  *      Author: santi
  */
-/*
+
 #include <gtkmm.h>
 
 #include "views/editor_MainWindow.h"
@@ -29,12 +29,5 @@ int main(int argc, char *argv[]) {
 	Gtk::Main::run(*mainWindow);
 
 	return EXIT_SUCCESS;
-	/*
-	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.examples.base");
-
-	Gtk::Window window;
-	window.set_default_size(1000, 1000);
-
-	return app->run(window);*/
 }
-*/
+
