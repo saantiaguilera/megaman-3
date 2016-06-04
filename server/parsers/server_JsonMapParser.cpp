9358c09d8f341949578476cb904619d9a6d5c72d
Martin Stancanelli <mastanca14@gmail.com>
2016-06-04 16:17:23 -0300

    Testing start game serializer

---
+++ server/parsers/server_JsonMapParser.cpp
@@ -0,0 +1,16 @@
+/*
+ * server_JsonMapParser.cpp
+ *
+ *  Created on: Jun 4, 2016
+ *      Author: mastanca
+ */
+
+#include "server_JsonMapParser.h"
+
+JsonMapParser::JsonMapParser(int mapNumber) {
+
+}
+
+JsonMapParser::~JsonMapParser() {
+}
+
