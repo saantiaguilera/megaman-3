909f4526099acff7fac2e26fa37f16618daccf27
Martin Stancanelli <mastanca14@gmail.com>
2016-06-05 17:38:23 -0300

    Add missing comma in position serializer

--- server/serializers/server_PositionSerializer.cpp
+++ server/serializers/server_PositionSerializer.cpp
@@ -23,6 +23,6 @@ PositionSerializer::~PositionSerializer() {
 std::string PositionSerializer::serializePosition() {
 	// { "id": ID, "type": TYPE, "position": { "x": 5, "y": 8 } }
 	std::stringstream ss;
-	ss << "{ \"id\": " << getObjectId() << ", \"type\": " << getObjectType() << "position\": { \"x\": " << x << ", \"y\": " << y << "} }";
+	ss << "{ \"id\": " << getObjectId() << ", \"type\": " << getObjectType() << ", position\": { \"x\": " << x << ", \"y\": " << y << "} }";
 	return ss.str();
 }
