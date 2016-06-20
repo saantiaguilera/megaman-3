c7398eff9425df19f8717d63e13fefad21de147a
santilazzari <santilazzari1994@gmail.com>
2016-06-20 17:39:32 -0300

    Fancy bullets

--- client/view/game_engine/client_ProjectileView.h
+++ client/view/game_engine/client_ProjectileView.h
@@ -6,46 +6,121 @@
 #include "../../../common/common_MapConstants.h"
 #include <SDL2pp/SDL2pp.hh>
 
+#define SPRITE_COUNT 4
+
+#define SPRITE_COUNT_EXTENDED 8
+
+#define IDLE_POSITION -1
+#define IDLE_PATH "res/drawable/bullets/idle.png"
+
 class ProjectileView : public AnimatedView {
 private:
-  SDL2pp::Texture *texture;
+  std::map<int, SDL2pp::Texture*> textureMap;
+  int firstLeft, firstRight, size, counter;
+
+  ORIENTATION lastOrientation;
 
 public:
   ProjectileView(unsigned int id, int type, SDL2pp::Renderer *renderer) : AnimatedView(id, renderer) {
     std::string path = "res/drawable/bullets/";
+    size = SPRITE_COUNT;
+
+    lastOrientation = IDLE;
+    counter = 0;
 
     switch (type) {
       case ObstacleViewTypeBomb:
-        path += "bomb";
+        path += "bombman/";
         break;
       case ObstacleViewTypeFire:
-        path += "fire";
+        path += "fireman/";
+        size = SPRITE_COUNT_EXTENDED;
         break;
       case ObstacleViewTypeMagnet:
-        path += "magnet";
+        path += "magnetman/";
+        size = SPRITE_COUNT_EXTENDED;
         break;
       case ObstacleViewTypeRing:
-        path += "ring";
+        path += "ringman/";
         break;
       case ObstacleViewTypeSpark:
-        path += "spark";
+        path += "sparkman/";
         break;
       case ObstacleViewTypePlasma:
-        path += "normal";
+        path += "normals/";
+        size = SPRITE_COUNT_EXTENDED;
         break;
     }
 
-    path += "_bullet.png";
+    path += "bullet";
 
-    texture = new SDL2pp::Texture(*getRenderer(), path);
+    if (size == SPRITE_COUNT) {
+      firstLeft = 1;
+      firstRight = 1;
+    }
+
+    if (size == SPRITE_COUNT_EXTENDED) {
+      firstRight = 1;
+      firstLeft = 5;
+    }
+
+    for (int i = 0 ; i < size ; ++i) {
+      std::stringstream ss;
+      ss << path << (i + 1) << ".png";
+      textureMap[i] = new SDL2pp::Texture(*getRenderer(), ss.str());
+    }
+
+    textureMap[IDLE_POSITION] = new SDL2pp::Texture(*getRenderer(), IDLE_PATH);
   }
 
   virtual ~ProjectileView() {
-    delete texture;
+    for (int i = 0 ; i < size ; ++i)
+      delete textureMap[i];
   }
 
   virtual SDL2pp::Texture * getTexture(ORIENTATION orient) {
-    return texture;
+    if (orient == lastOrientation) {
+      counter++;
+      switch (orient) {
+        case UP:
+        case DOWN:
+        case IDLE:
+          currentSprite = IDLE_POSITION;
+          break;
+        case LEFT:
+          spriteStep();
+          if (counter > SPRITE_COUNT) {
+            currentSprite = firstLeft;
+            counter = 0;
+          }
+          break;
+        case RIGHT:
+          spriteStep();
+          if (counter > SPRITE_COUNT) {
+            currentSprite = firstRight;
+            counter = 0;
+          }
+          break;
+      }
+    } else {
+      counter = 0;
+      switch (orient) {
+        case UP:
+        case DOWN:
+        case IDLE:
+          break;
+        case LEFT:
+          currentSprite = firstLeft;
+          break;
+        case RIGHT:
+          currentSprite = firstRight;
+          break;
+      }
+    }
+
+    lastOrientation = orient;
+
+    return textureMap[currentSprite];
   }
 
 };
